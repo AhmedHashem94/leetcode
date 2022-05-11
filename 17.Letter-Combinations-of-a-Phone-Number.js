17d52287219cc732f7cbde3db5d655a406a6cd0e
Ahmed Hashem <aabdelhalim@youxel.com>
2022-05-12 01:07:51 +0200

    first commit => add 5 problems

---
+++ 17.Letter-Combinations-of-a-Phone-Number.js
@@ -0,0 +1,62 @@
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+// A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+// Example 1:
+
+// Input: digits = "23"
+// Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+// Example 2:
+
+// Input: digits = ""
+// Output: []
+
+// Example 3:
+// Input: digits = "2"
+// Output: ["a","b","c"]
+
+// Constraints:
+
+// 0 <= digits.length <= 4
+// digits[i] is a digit in the range ['2', '9'].
+
+const letterCombinations = function (digits) {
+  // return empty array if empty string
+  if (digits.length === 0) return [];
+
+  // global result
+  const result = [];
+
+  // map foreach button
+  const map = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
+  };
+
+  const dfs = (i, digits, slate) => {
+    // base case
+    if (i === digits.length) {
+      result.push(slate.join(''));
+      return;
+    }
+
+    let chars = map[digits[i]];
+
+    for (let char of chars) {
+      slate.push(char);
+      dfs(i + 1, digits, slate);
+      slate.pop();
+    }
+  };
+
+  dfs(0, digits, []);
+  return result;
+};
+
+console.log(letterCombinations('234'));
